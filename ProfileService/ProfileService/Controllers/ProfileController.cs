34eef9a03546dad434954fcfd77b632f13660324
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2024-10-07 17:18:15 +0300

    Create profile controller with creation endpoint

---
+++ ProfileService/ProfileService/Controllers/ProfileController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ProfileService.BLL.Dto;
+using ProfileService.BLL.Services.IServices;
+
+namespace ProfileService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProfileController(IUserProfileService profileService) : ControllerBase
+    {
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> CreateProfile(UserRegistrationData userData, CancellationToken cancellationToken)
+        {
+            var profile = await profileService.CreateProfileAsync(userData, cancellationToken);
+
+            return Ok(profile);
+        }
+    }
+}
