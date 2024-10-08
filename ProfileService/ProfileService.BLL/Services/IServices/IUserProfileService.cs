6fcd1ed43996e7afbfe6d7f0e027416e4de93bcd
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2024-10-08 15:47:45 +0300

    Create endpoint for getting own profile

--- ProfileService/ProfileService.BLL/Services/IServices/IUserProfileService.cs
+++ ProfileService/ProfileService.BLL/Services/IServices/IUserProfileService.cs
@@ -8,5 +8,7 @@ namespace ProfileService.BLL.Services.IServices
         public Task<UserProfile> CreateProfileAsync(UserRegistrationData userData, CancellationToken cancellationToken = default);
 
         public Task<UserProfile> GetProfileAsync(Guid id, CancellationToken cancellationToken = default);
+
+        public Task<UserProfile> GetOwnProfileAsync(string auth0Id, CancellationToken cancellationToken = default);
     }
 }
--- ProfileService/ProfileService/Controllers/ProfileController.cs
+++ ProfileService/ProfileService/Controllers/ProfileController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using ProfileService.BLL.Dto;
 using ProfileService.BLL.Services.IServices;
 using ProfileService.DAL.Models;
+using System.Security.Claims;
 
 namespace ProfileService.Controllers
 {
@@ -19,10 +20,20 @@ namespace ProfileService.Controllers
             return profile;
         }
 
+        [Authorize]
         [HttpGet("{id}")]
         public async Task<UserProfile> GetUserProfile(Guid id, CancellationToken cancellationToken)
         {
             return await profileService.GetProfileAsync(id, cancellationToken);
+        }
+
+        [Authorize]
+        [HttpGet("my")]
+        public async Task<UserProfile> GetOwnProfile(CancellationToken cancellationToken)
+        {
+            var auth0Id = HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)!.Value;
+
+            return await profileService.GetOwnProfileAsync(auth0Id, cancellationToken);
         }
     }
 }
