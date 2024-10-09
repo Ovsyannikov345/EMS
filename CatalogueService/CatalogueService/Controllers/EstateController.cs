690cae6ee1ed5e37fa7f8d11843dae9386b7b0ae
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2024-10-09 17:05:23 +0300

    Add endpoint for fetching estate list

--- CatalogueService/CatalogueService/Controllers/EstateController.cs
+++ CatalogueService/CatalogueService/Controllers/EstateController.cs
@@ -1,5 +1,6 @@
 ﻿using CatalogueService.BLL.Dto;
 using CatalogueService.BLL.Services.IServices;
+using CatalogueService.DAL.Models.Entities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,11 +10,18 @@ namespace CatalogueService.Controllers
     [Route("api/[controller]")]
     public class EstateController(IEstateService estateService) : ControllerBase
     {
-        [HttpGet("{id}")]
-        [AllowAnonymous]
-        public async Task<EstateFullDetails> GetEstateData(Guid id)
+        [HttpGet]
+        [Authorize]
+        public async Task<IEnumerable<Estate>> GetEstateList(CancellationToken cancellationToken)
         {
-            return await estateService.GetEstateDetails(id);
+            return await estateService.GetEstateList(cancellationToken);
+        }
+
+        [HttpGet("{id}")]
+        [Authorize]
+        public async Task<EstateFullDetails> GetEstateData(Guid id, CancellationToken cancellationToken)
+        {
+            return await estateService.GetEstateDetails(id, cancellationToken);
         }
     }
 }
