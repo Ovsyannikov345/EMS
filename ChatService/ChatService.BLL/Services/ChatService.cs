e2a8ce7e6c13de2bf31b05daf5d0cfeed92dbeef
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2024-10-15 14:08:55 +0300

    Add mesasges and user data to chat getting

--- ChatService/ChatService.BLL/Services/ChatService.cs
+++ ChatService/ChatService.BLL/Services/ChatService.cs
@@ -8,11 +8,15 @@ using ChatService.DAL.Repositories.IRepositories;
 
 namespace ChatService.BLL.Services
 {
-    public class ChatService(IChatRepository chatRepository, IEstateGrpcClient estateGrpcClient, IMapper mapper) : IChatService
+    public class ChatService(
+        IChatRepository chatRepository,
+        IEstateGrpcClient estateGrpcClient,
+        IProfileGrpcClient profileGrpcClient,
+        IMapper mapper) : IChatService
     {
         public async Task<ChatModel> GetChatAsync(Guid id, CancellationToken cancellationToken = default)
         {
-            var chat = await chatRepository.GetByFilterAsync(c => c.Id == id, cancellationToken);
+            var chat = await chatRepository.GetChatWithMessages(id, cancellationToken);
 
             if (chat == null)
             {
@@ -24,6 +28,18 @@ namespace ChatService.BLL.Services
             var chatModel = mapper.Map<ChatModel>(chat);
 
             chatModel.Estate = mapper.Map<EstateModel>(estateResponse.Estate);
+
+            var profileResponse = await profileGrpcClient.GetProfile(chat.UserId, cancellationToken);
+
+            chatModel.User = new UserProfileModel
+            {
+                Id = Guid.Parse(profileResponse.Profile.Id),
+                Auth0Id = profileResponse.Profile.Auth0Id,
+                FirstName = profileResponse.Profile.FirstName,
+                LastName = profileResponse.Profile.LastName,
+                PhoneNumber = profileResponse.Profile.PhoneNumber,
+                BirthDate = profileResponse.Profile.BirthDate.ToDateTime()
+            };
 
             return chatModel;
         }
