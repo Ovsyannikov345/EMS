33ceabf3c3a2f887469cfd8807b996cb0bd59ad0
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2024-10-15 13:56:16 +0300

    Add method for getting chat with messages

--- ChatService/ChatService.DAL/Repositories/ChatRepository.cs
+++ ChatService/ChatService.DAL/Repositories/ChatRepository.cs
@@ -1,8 +1,15 @@
 ﻿using ChatService.DAL.Data;
 using ChatService.DAL.Models.Entities;
 using ChatService.DAL.Repositories.IRepositories;
+using Microsoft.EntityFrameworkCore;
 
 namespace ChatService.DAL.Repositories
 {
-    public class ChatRepository(ChatDbContext context) : GenericRepository<Chat>(context), IChatRepository;
+    public class ChatRepository(ChatDbContext context) : GenericRepository<Chat>(context), IChatRepository
+    {
+        public async Task<Chat?> GetChatWithMessages(Guid chatId, CancellationToken cancellationToken = default)
+        {
+            return await context.Chats.AsNoTracking().Include(c => c.Messages).FirstOrDefaultAsync(c => c.Id == chatId, cancellationToken);
+        }
+    }
 }
--- ChatService/ChatService.DAL/Repositories/IRepositories/IChatRepository.cs
+++ ChatService/ChatService.DAL/Repositories/IRepositories/IChatRepository.cs
@@ -2,5 +2,8 @@
 
 namespace ChatService.DAL.Repositories.IRepositories
 {
-    public interface IChatRepository : IGenericRepository<Chat>;
+    public interface IChatRepository : IGenericRepository<Chat>
+    {
+        public Task<Chat?> GetChatWithMessages(Guid chatId, CancellationToken cancellationToken = default);
+    }
 }
