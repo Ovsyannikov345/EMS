e4e8cc389d4d2e1aa1a96dd7306f455e1c6af9aa
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2024-10-15 14:09:42 +0300

    Add view models with mapping

---
+++ ChatService/ChatService/Utilities/Mapping/AutoMapperProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using ChatService.BLL.Models;
+using ChatService.ViewModels;
+
+namespace ChatService.Utilities.Mapping
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<ChatModel, ChatViewModel>();
+            CreateMap<EstateModel, EstateViewModel>();
+            CreateMap<UserProfileModel, UserProfileViewModel>();
+            CreateMap<MessageModel, MessageViewModel>();
+        }
+    }
+}
---
+++ ChatService/ChatService/ViewModels/MessageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using ChatService.BLL.Models;
+
+namespace ChatService.ViewModels
+{
+    public class MessageViewModel
+    {
+        public Guid Id { get; set; }
+
+        public string Text { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public Guid ChatId { get; set; }
+
+        public ChatViewModel Chat { get; set; } = null!;
+    }
+}
