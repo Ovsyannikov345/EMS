85b5aba9b73689d35e8f2639ed92cfb93ddddd60
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2024-10-18 15:51:48 +0300

    Create entities and database context

---
+++ NotificationService/NotificationService.DAL/DI/ServicesConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NotificationService.DAL.Data;
+
+namespace NotificationService.DAL.DI
+{
+    public static class ServicesConfiguration
+    {
+        public static void AddDataAccessDependencies(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<NotificationDbContext>(options =>
+                options.UseNpgsql(configuration.GetConnectionString("DbConnection")));
+        }
+    }
+}
