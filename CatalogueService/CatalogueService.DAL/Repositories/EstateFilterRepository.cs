d5595e6d430ec5924ec26691798a417db9ea708b
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2024-10-23 11:33:08 +0300

    Create estate filter repository

---
+++ CatalogueService/CatalogueService.DAL/Repositories/EstateFilterRepository.cs
@@ -0,0 +1,10 @@
+﻿using CatalogueService.DAL.Data;
+using CatalogueService.DAL.Models.Entities;
+using CatalogueService.DAL.Repositories.IRepositories;
+
+namespace CatalogueService.DAL.Repositories
+{
+    public class EstateFilterRepository(EstateDbContext context) : GenericRepository<EstateFilter>(context), IEstateFilterRepository
+    {
+    }
+}
