3c7a81f11565c5f29c713a42466cd986cbd3a4d0
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2024-10-23 14:46:44 +0300

    Create estate filter controller

---
+++ CatalogueService/CatalogueService/Controllers/EstateFilterController.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using CatalogueService.BLL.Models;
+using CatalogueService.BLL.Services.IServices;
+using CatalogueService.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace CatalogueService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class EstateFilterController(IEstateFilterService estateFilterService, IMapper mapper) : ControllerBase
+    {
+        [HttpGet("my")]
+        public async Task<EstateFilterViewModel> GetEstateFilter(CancellationToken cancellationToken)
+        {
+            var filter = await estateFilterService.GetEstateFilterAsync(GetAuth0IdFromContext(), cancellationToken);
+
+            return mapper.Map<EstateFilterViewModel>(filter);
+        }
+
+        [HttpPost]
+        public async Task<EstateFilterViewModel> CreateEstateFilter(EstateFilterToCreateViewModel filterData, CancellationToken cancellationToken)
+        {
+            var filter = await estateFilterService.CreateEstateFilterAsync(GetAuth0IdFromContext(), mapper.Map<EstateFilterModel>(filterData), cancellationToken);
+
+            return mapper.Map<EstateFilterViewModel>(filter);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<EstateFilterViewModel> UpdateEstateFilter(Guid id, EstateFilterViewModel filterData, CancellationToken cancellationToken)
+        {
+            var userAuth0Id = GetAuth0IdFromContext();
+
+            var updatedFilter = await estateFilterService.UpdateEstateFilterAsync(id, userAuth0Id, mapper.Map<EstateFilterModel>(filterData), cancellationToken);
+
+            return mapper.Map<EstateFilterViewModel>(updatedFilter);
+        }
+
+        private string GetAuth0IdFromContext()
+        {
+            return HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)!.Value;
+        }
+    }
+}
