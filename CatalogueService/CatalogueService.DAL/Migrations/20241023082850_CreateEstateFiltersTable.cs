ae6044cfb5423ea5e99390517a5ad1db881eb6d2
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2024-10-23 11:29:48 +0300

    Add estate filter entity and migrate

---
+++ CatalogueService/CatalogueService.DAL/Migrations/20241023082850_CreateEstateFiltersTable.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CatalogueService.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateEstateFiltersTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EstateFilters",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    EstateTypes = table.Column<int[]>(type: "integer[]", nullable: false),
+                    MinArea = table.Column<int>(type: "integer", nullable: false),
+                    MaxArea = table.Column<int>(type: "integer", nullable: false),
+                    MinRoomsCount = table.Column<short>(type: "smallint", nullable: false),
+                    MaxRoomsCount = table.Column<short>(type: "smallint", nullable: false),
+                    MinPrice = table.Column<decimal>(type: "numeric", nullable: false),
+                    MaxPrice = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EstateFilters", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EstateFilters");
+        }
+    }
+}
---
+++ CatalogueService/CatalogueService.DAL/Models/Entities/EstateFilter.cs
@@ -0,0 +1,25 @@
+﻿using CatalogueService.DAL.Models.Enums;
+
+namespace CatalogueService.DAL.Models.Entities
+{
+    public class EstateFilter
+    {
+        public Guid Id { get; set; }
+
+        public Guid UserId { get; set; }
+
+        public ICollection<EstateType> EstateTypes { get; set; } = [];
+
+        public int MinArea { get; set; }
+
+        public int MaxArea { get; set; }
+
+        public short MinRoomsCount { get; set; }
+
+        public short MaxRoomsCount { get; set; }
+
+        public decimal MinPrice { get; set; }
+
+        public decimal MaxPrice { get; set; }
+    }
+}
