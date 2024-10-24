851ebd733c9b6d5732ff8c761a64894cd5c6b2e2
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2024-10-24 17:14:57 +0300

    Create data generators

---
+++ ProfileService/ProfileService.UnitTests/DataGeneration/DataGenerator.cs
@@ -0,0 +1,26 @@
+﻿using Bogus;
+using ProfileService.DAL.Models;
+using ProfileService.DAL.Models.Enums;
+
+namespace ProfileService.UnitTests.Datageneration
+{
+    internal static class DataGenerator
+    {
+        public static List<UserProfile> GenerateProfiles(int count) =>
+            new Faker<UserProfile>()
+                .RuleFor(p => p.Id, _ => Guid.NewGuid())
+                .RuleFor(p => p.Auth0Id, f => "auth0|" + f.Random.String2(24, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"))
+                .RuleFor(p => p.FirstName, f => f.Name.FirstName())
+                .RuleFor(p => p.LastName, f => f.Name.LastName())
+                .RuleFor(p => p.PhoneNumber, f => f.Phone.PhoneNumber())
+                .RuleFor(p => p.BirthDate, f => f.Date.Between(DateTime.UtcNow.AddYears(-90), DateTime.UtcNow))
+                .Generate(count);
+
+        public static List<ProfileInfoVisibility> GenerateProfileVisibilities(int count) =>
+            new Faker<ProfileInfoVisibility>()
+                .RuleFor(p => p.Id, _ => Guid.NewGuid())
+                .RuleFor(p => p.PhoneNumberVisibility, f => f.PickRandom<InfoVisibility>())
+                .RuleFor(p => p.BirthDateVisibility, f => f.PickRandom<InfoVisibility>())
+                .Generate(count);
+    }
+}
