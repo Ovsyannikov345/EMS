7a6c8dc9090f39b79b235cd40e5a9e6e0c9a80e8
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2024-10-24 17:15:10 +0300

    Create repository mocks

---
+++ ProfileService/ProfileService.UnitTests/Mocks/ProfileInfoVisibilityRepositoryMock.cs
@@ -0,0 +1,48 @@
+﻿using Moq;
+using ProfileService.DAL.Models;
+using ProfileService.DAL.Repositories.IRepositories;
+using System.Linq.Expressions;
+
+namespace ProfileService.UnitTests.Mocks
+{
+    internal class ProfileInfoVisibilityRepositoryMock : Mock<IProfileInfoVisibilityRepository>
+    {
+        private readonly CancellationToken _anyToken = It.IsAny<CancellationToken>();
+
+        public void GetByIdAsync(ProfileInfoVisibility profileInfoVisibilityToReturn) =>
+            Setup(repo => repo.GetByIdAsync(It.IsAny<Guid>(), _anyToken))
+                .ReturnsAsync(profileInfoVisibilityToReturn);
+
+        public void GetByIdAsyncReturnsNull() =>
+            Setup(repo => repo.GetByIdAsync(It.IsAny<Guid>(), _anyToken))
+                .ReturnsAsync((ProfileInfoVisibility?)null);
+
+        public void GetByFilterAsync(ProfileInfoVisibility profileInfoVisibilityToReturn) =>
+            Setup(repo => repo.GetByFilterAsync(It.IsAny<Expression<Func<ProfileInfoVisibility, bool>>>(), _anyToken))
+                .ReturnsAsync(profileInfoVisibilityToReturn);
+
+        public void GetByFilterAsyncReturnsNull() =>
+            Setup(repo => repo.GetByFilterAsync(It.IsAny<Expression<Func<ProfileInfoVisibility, bool>>>(), _anyToken))
+                .ReturnsAsync((ProfileInfoVisibility?)null);
+
+        public void GetAllAsync(IEnumerable<ProfileInfoVisibility> profileInfoVisibilitiesToReturn) =>
+            Setup(repo => repo.GetAllAsync(_anyToken))
+                .ReturnsAsync(profileInfoVisibilitiesToReturn);
+
+        public void CreateAsync(ProfileInfoVisibility createdProfileInfoVisibility) =>
+            Setup(repo => repo.CreateAsync(It.IsAny<ProfileInfoVisibility>(), _anyToken))
+                .ReturnsAsync(createdProfileInfoVisibility);
+
+        public void UpdateAsync(ProfileInfoVisibility updatedProfileInfoVisibility) =>
+            Setup(repo => repo.UpdateAsync(It.IsAny<ProfileInfoVisibility>(), _anyToken))
+                .ReturnsAsync(updatedProfileInfoVisibility);
+
+        public void DeleteAsync(ProfileInfoVisibility deletedProfileInfoVisibility) =>
+            Setup(repo => repo.DeleteAsync(It.IsAny<Guid>(), _anyToken))
+                .ReturnsAsync(deletedProfileInfoVisibility);
+
+        public void DeleteAsyncReturnsNull() =>
+            Setup(repo => repo.DeleteAsync(It.IsAny<Guid>(), _anyToken))
+                .ReturnsAsync((ProfileInfoVisibility?)null);
+    }
+}
---
+++ ProfileService/ProfileService.UnitTests/Mocks/ProfileRepositoryMock.cs
@@ -0,0 +1,48 @@
+﻿using Moq;
+using ProfileService.DAL.Models;
+using ProfileService.DAL.Repositories.IRepositories;
+using System.Linq.Expressions;
+
+namespace ProfileService.UnitTests.Mocks;
+
+internal class ProfileRepositoryMock : Mock<IProfileRepository>
+{
+    private readonly CancellationToken _anyToken = It.IsAny<CancellationToken>();
+
+    public void GetByIdAsync(UserProfile userProfileToReturn) =>
+        Setup(repo => repo.GetByIdAsync(It.IsAny<Guid>(), _anyToken))
+            .ReturnsAsync(userProfileToReturn);
+
+    public void GetByIdAsyncReturnsNull() =>
+        Setup(repo => repo.GetByIdAsync(It.IsAny<Guid>(), _anyToken))
+            .ReturnsAsync((UserProfile?)null);
+
+    public void GetByFilterAsync(UserProfile userProfileToReturn) =>
+        Setup(repo => repo.GetByFilterAsync(It.IsAny<Expression<Func<UserProfile, bool>>>(), _anyToken))
+            .ReturnsAsync(userProfileToReturn);
+
+    public void GetByFilterAsyncReturnsNull() =>
+        Setup(repo => repo.GetByFilterAsync(It.IsAny<Expression<Func<UserProfile, bool>>>(), _anyToken))
+            .ReturnsAsync((UserProfile?)null);
+
+    public void GetAllAsync(IEnumerable<UserProfile> userProfilesToReturn) =>
+        Setup(repo => repo.GetAllAsync(_anyToken))
+            .ReturnsAsync(userProfilesToReturn);
+
+    public void CreateAsync(UserProfile createdUserProfile) =>
+        Setup(repo => repo.CreateAsync(It.IsAny<UserProfile>(), _anyToken))
+            .ReturnsAsync(createdUserProfile);
+
+    public void UpdateAsync(UserProfile updatedUserProfile) =>
+        Setup(repo => repo.UpdateAsync(It.IsAny<UserProfile>(), _anyToken))
+            .ReturnsAsync(updatedUserProfile);
+
+    public void DeleteAsync(UserProfile deletedUserProfile) =>
+        Setup(repo => repo.DeleteAsync(It.IsAny<Guid>(), _anyToken))
+            .ReturnsAsync(deletedUserProfile);
+
+    public void DeleteAsyncReturnsNull() =>
+        Setup(repo => repo.DeleteAsync(It.IsAny<Guid>(), _anyToken))
+            .ReturnsAsync((UserProfile?)null);
+}
+
