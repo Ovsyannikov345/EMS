c5c82d9065d544224e7bb27e6c3c92d08f8b81e6
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2024-10-25 16:32:09 +0300

    Create tests for profile service

---
+++ ProfileService/ProfileService.UnitTests/Utilities/Mapping/TestsAutoMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ProfileService.BLL.Models;
+
+namespace ProfileService.UnitTests.Utilities.Mapping
+{
+    internal class TestsAutoMapperProfile : Profile
+    {
+        public TestsAutoMapperProfile()
+        {
+            CreateMap<UserProfileModel, RegistrationDataModel>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Auth0Id));
+        }
+    }
+}
