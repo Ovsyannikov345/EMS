6efe93a322dd367276c71c8df8ad56108f9f1ab0
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2024-10-25 16:06:52 +0300

    change data generation logic

--- ProfileService/ProfileService.UnitTests/DataGeneration/DataGenerator.cs
+++ ProfileService/ProfileService.UnitTests/DataGeneration/DataGenerator.cs
@@ -6,21 +6,37 @@ namespace ProfileService.UnitTests.Datageneration
 {
     internal static class DataGenerator
     {
-        public static List<UserProfile> GenerateProfiles(int count) =>
+        public static List<UserProfile> GenerateUserProfiles(int count)
+        {
+            var profileFaker = GetProfileFaker();
+
+            var profileVisibilityFaker = GetProfileVisibilityFaker();
+
+            var profiles = profileFaker.Generate(count);
+
+            foreach (var profile in profiles)
+            {
+                profile.InfoVisibility = profileVisibilityFaker.Generate();
+                profile.InfoVisibility.UserId = profile.Id;
+                profile.InfoVisibility.User = profile;
+            }
+
+            return profiles;
+        }
+
+        private static Faker<UserProfile> GetProfileFaker() =>
             new Faker<UserProfile>()
                 .RuleFor(p => p.Id, _ => Guid.NewGuid())
                 .RuleFor(p => p.Auth0Id, f => "auth0|" + f.Random.String2(24, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"))
                 .RuleFor(p => p.FirstName, f => f.Name.FirstName())
                 .RuleFor(p => p.LastName, f => f.Name.LastName())
                 .RuleFor(p => p.PhoneNumber, f => f.Phone.PhoneNumber())
-                .RuleFor(p => p.BirthDate, f => f.Date.Between(DateTime.UtcNow.AddYears(-90), DateTime.UtcNow))
-                .Generate(count);
+                .RuleFor(p => p.BirthDate, f => f.Date.Between(DateTime.UtcNow.AddYears(-90), DateTime.UtcNow));
 
-        public static List<ProfileInfoVisibility> GenerateProfileVisibilities(int count) =>
+        private static Faker<ProfileInfoVisibility> GetProfileVisibilityFaker() =>
             new Faker<ProfileInfoVisibility>()
                 .RuleFor(p => p.Id, _ => Guid.NewGuid())
                 .RuleFor(p => p.PhoneNumberVisibility, f => f.PickRandom<InfoVisibility>())
-                .RuleFor(p => p.BirthDateVisibility, f => f.PickRandom<InfoVisibility>())
-                .Generate(count);
+                .RuleFor(p => p.BirthDateVisibility, f => f.PickRandom<InfoVisibility>());
     }
 }
