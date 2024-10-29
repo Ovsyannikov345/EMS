d4f65e72fdd7d5c09131b4e7da723d42114b4b97
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2024-10-29 13:21:02 +0300

    Add tests for getting notification list

--- NotificationService/NotificationService.Tests/ServicesTests/NotificationServiceTests.cs
+++ NotificationService/NotificationService.Tests/ServicesTests/NotificationServiceTests.cs
@@ -2,11 +2,16 @@
 using AutoMapper;
 using FluentAssertions;
 using NotificationService.BLL.Models;
+using NotificationService.BLL.Utilities.Exceptions;
 using NotificationService.BLL.Utilities.Mapping;
+using NotificationService.DAL.Grpc.Services.IServices;
+using NotificationService.DAL.Grpc.Services.Profile;
 using NotificationService.DAL.Models.Entities;
 using NotificationService.DAL.Repositories.IRepositories;
 using NotificationService.Tests.DataInjection;
 using NSubstitute;
+using NSubstitute.ReturnsExtensions;
+using System.Linq.Expressions;
 
 namespace NotificationService.Tests.ServicesTests
 {
@@ -31,6 +36,45 @@ namespace NotificationService.Tests.ServicesTests
 
             // Assert
             result.Should().BeEquivalentTo(notificationModel);
+        }
+
+        [Theory]
+        [AutoDomainData]
+        public async Task GetNotificationListAsync_ProfileNotExists_ThrowsNotFoundException(
+            [Frozen] IProfileGrpcClient profileGrpcClientMock,
+            NotificationService.BLL.Services.NotificationService sut)
+        {
+            // Arrange
+            profileGrpcClientMock.GetOwnProfile(Arg.Any<string>(), Arg.Any<CancellationToken>())
+                .ReturnsNull();
+
+            // Act
+            var result = async () => await sut.GetNotificationListAsync("", default);
+
+            // Assert
+            await result.Should().ThrowAsync<NotFoundException>();
+        }
+
+        [Theory]
+        [AutoDomainData]
+        public async Task GetNotificationListAsync_ValidProfile_ReturnsNotificationList(
+            [Frozen] INotificationRepository notificationRepositoryMock,
+            [Frozen] IProfileGrpcClient profileGrpcClientMock,
+            List<NotificationModel> notificationModelList,
+            ProfileResponse profileResponse,
+            NotificationService.BLL.Services.NotificationService sut)
+        {
+            // Arrange
+            profileGrpcClientMock.GetOwnProfile(Arg.Any<string>(), Arg.Any<CancellationToken>())
+                .Returns(profileResponse);
+            notificationRepositoryMock.GetAllAsync(Arg.Any<Expression<Func<Notification, bool>>>(), Arg.Any<CancellationToken>())
+                .Returns(_mapper.Map<IEnumerable<NotificationModel>, IEnumerable<Notification>>(notificationModelList));
+
+            // Act
+            var result = await sut.GetNotificationListAsync(profileResponse.Profile.Auth0Id, default);
+
+            // Assert
+            result.Should().BeEquivalentTo(notificationModelList);
         }
     }
 }
