b5a724bd39f4ba88288a7fc51327df7330e9eb26
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2024-10-29 13:13:31 +0300

    Add notification creation tests

---
+++ NotificationService/NotificationService.Tests/DataInjection/AutoDomainDataAttribute.cs
@@ -0,0 +1,18 @@
+﻿using AutoFixture;
+using AutoFixture.AutoNSubstitute;
+using AutoFixture.Community.AutoMapper;
+using AutoFixture.Xunit2;
+using NotificationService.BLL.Utilities.Mapping;
+
+namespace NotificationService.Tests.DataInjection
+{
+    internal class AutoDomainDataAttribute : AutoDataAttribute
+    {
+        public AutoDomainDataAttribute()
+            : base(() => new Fixture()
+                .Customize(new AutoNSubstituteCustomization())
+                .Customize(new AutoMapperCustomization(x => x.AddMaps(typeof(AutoMapperProfile)))))
+        {
+        }
+    }
+}
---
+++ NotificationService/NotificationService.Tests/ServicesTests/NotificationServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using AutoFixture.Xunit2;
+using AutoMapper;
+using FluentAssertions;
+using NotificationService.BLL.Models;
+using NotificationService.BLL.Utilities.Mapping;
+using NotificationService.DAL.Models.Entities;
+using NotificationService.DAL.Repositories.IRepositories;
+using NotificationService.Tests.DataInjection;
+using NSubstitute;
+
+namespace NotificationService.Tests.ServicesTests
+{
+    public class NotificationServiceTests
+    {
+        private readonly Mapper _mapper = new(new MapperConfiguration(mc =>
+            mc.AddProfile(new AutoMapperProfile())));
+
+        [Theory]
+        [AutoDomainData]
+        public async Task CreateNotificationAsync_ValidNotification_ReturnsCreatedNotification(
+            [Frozen] INotificationRepository notificationRepositoryMock,
+            NotificationModel notificationModel,
+            NotificationService.BLL.Services.NotificationService sut)
+        {
+            // Arrange
+            notificationRepositoryMock.CreateAsync(Arg.Any<Notification>(), Arg.Any<CancellationToken>())
+                .Returns(_mapper.Map<Notification>(notificationModel));
+
+            // Act
+            var result = await sut.CreateNotificationAsync(notificationModel, default);
+
+            // Assert
+            result.Should().BeEquivalentTo(notificationModel);
+        }
+    }
+}
