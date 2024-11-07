4c18a1336aa510a45d05517023a20c006a5b4460
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2024-11-08 00:45:10 +0300

    Add grpc support for profile service

--- ProfileService/ProfileService.DAL/DataContext/ProfileDbContext.cs
+++ ProfileService/ProfileService.DAL/DataContext/ProfileDbContext.cs
@@ -3,11 +3,19 @@ using ProfileService.DAL.Models;
 
 namespace ProfileService.DAL.DataContext
 {
-    public class ProfileDbContext(DbContextOptions<ProfileDbContext> options) : DbContext(options)
+    public class ProfileDbContext : DbContext
     {
         static ProfileDbContext()
         {
             AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+        }
+
+        public ProfileDbContext(DbContextOptions options) : base(options)
+        {
+            if (Database.IsRelational())
+            {
+                Database.Migrate();
+            }
         }
 
         public DbSet<UserProfile> Profiles { get; set; }
