3627eb721ae47a02829e916f47effe04a9d43aa6
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2024-11-08 11:44:47 +0300

    Add db migration on startup

--- NotificationService/NotificationService.DAL/Data/NotificationDbContext.cs
+++ NotificationService/NotificationService.DAL/Data/NotificationDbContext.cs
@@ -3,8 +3,16 @@ using NotificationService.DAL.Models.Entities;
 
 namespace NotificationService.DAL.Data
 {
-    public class NotificationDbContext(DbContextOptions<NotificationDbContext> context) : DbContext(context)
+    public class NotificationDbContext : DbContext
     {
+        public NotificationDbContext(DbContextOptions options) : base(options)
+        {
+            if (Database.IsRelational())
+            {
+                Database.Migrate();
+            }
+        }
+
         public DbSet<Notification> Notifications { get; set; }
     }
 }
