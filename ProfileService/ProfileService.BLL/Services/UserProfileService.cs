7b99374393fffd4edc865533e22cabbdfd76bb43
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2024-11-11 14:40:05 +0300

    Use manager for profile service

--- ProfileService/ProfileService.BLL/Services/UserProfileService.cs
+++ ProfileService/ProfileService.BLL/Services/UserProfileService.cs
@@ -3,7 +3,7 @@ using ProfileService.BLL.Models;
 using ProfileService.BLL.Services.IServices;
 using ProfileService.BLL.Utilities.Exceptions;
 using ProfileService.BLL.Utilities.Exceptions.Messages;
-using ProfileService.DAL.CacheProvider.ICacheProvider;
+using ProfileService.DAL.CacheRepositoryManagers.ICacheRepositoryManagers;
 using ProfileService.DAL.Models;
 using ProfileService.DAL.Models.Enums;
 using ProfileService.DAL.Repositories.IRepositories;
@@ -14,7 +14,8 @@ namespace ProfileService.BLL.Services
         IProfileRepository profileRepository,
         IProfileInfoVisibilityRepository visibilityRepository,
         IMapper mapper,
-        IRedisCacheProvider redisCache) : IUserProfileService
+        ICacheRepositoryManager<UserProfile> userCacheRepositoryManager,
+        ICacheRepositoryManager<ProfileInfoVisibility> visibilityCacheRepositoryManager) : IUserProfileService
     {
         public async Task<UserProfileModel> CreateProfileAsync(RegistrationDataModel userData, CancellationToken cancellationToken = default)
         {
@@ -34,29 +35,14 @@ namespace ProfileService.BLL.Services
 
         public async Task<UserProfileModelWithPrivacy> GetProfileAsync(Guid id, CancellationToken cancellationToken = default)
         {
-            var profileKey = nameof(UserProfile) + id;
-
-            var userProfile = await redisCache.GetDataFromCache<UserProfile>(profileKey);
-
-            if (userProfile is null)
-            {
-                userProfile = await profileRepository.GetByFilterAsync(p => p.Id == id, cancellationToken)
-                    ?? throw new NotFoundException(ExceptionMessages.NotFound(nameof(UserProfile), nameof(UserProfile.Id), id));
-
-                await redisCache.CacheData(userProfile, TimeSpan.FromMinutes(10), profileKey);
-            }
+            var userProfile = await userCacheRepositoryManager.GetEntityByIdAsync(id, cancellationToken: cancellationToken)
+                ?? throw new NotFoundException(ExceptionMessages.NotFound(nameof(UserProfile), nameof(UserProfile.Id), id));
 
             var visibilityKey = nameof(ProfileInfoVisibility) + id;
 
-            var visibilityOptions = await redisCache.GetDataFromCache<ProfileInfoVisibility>(visibilityKey);
-
-            if (visibilityOptions is null)
-            {
-                visibilityOptions = await visibilityRepository.GetByFilterAsync(v => v.UserId == userProfile.Id, cancellationToken)
-                    ?? throw new NotFoundException(ExceptionMessages.NotFound(nameof(ProfileInfoVisibility), nameof(ProfileInfoVisibility.UserId), userProfile.Id));
-
-                await redisCache.CacheData(visibilityOptions, TimeSpan.FromMinutes(10), visibilityKey);
-            }
+            var visibilityOptions = await visibilityCacheRepositoryManager.GetEntityByFilterAsync(
+                visibilityKey, v => v.UserId == userProfile.Id, cancellationToken: cancellationToken)
+                ?? throw new NotFoundException(ExceptionMessages.NotFound(nameof(ProfileInfoVisibility), nameof(ProfileInfoVisibility.UserId), userProfile.Id));
 
             var profileModel = mapper.Map<UserProfileModelWithPrivacy>(userProfile);
 
@@ -75,17 +61,9 @@ namespace ProfileService.BLL.Services
 
         public async Task<UserProfileModel> GetOwnProfileAsync(string auth0Id, CancellationToken cancellationToken = default)
         {
-            var profileKey = nameof(UserProfile.Auth0Id) + auth0Id;
-
-            var userProfile = await redisCache.GetDataFromCache<UserProfile>(profileKey);
-
-            if (userProfile is null)
-            {
-                userProfile = await profileRepository.GetByFilterAsync(p => p.Auth0Id == auth0Id, cancellationToken)
-                    ?? throw new NotFoundException(ExceptionMessages.NotFound(nameof(UserProfile), nameof(UserProfile.Auth0Id), auth0Id));
-
-                await redisCache.CacheData(userProfile, TimeSpan.FromMinutes(10), profileKey);
-            }
+            var userProfile = await userCacheRepositoryManager.GetEntityByFilterAsync(
+                nameof(UserProfile.Auth0Id) + auth0Id, p => p.Auth0Id == auth0Id, cancellationToken: cancellationToken)
+                ?? throw new NotFoundException(ExceptionMessages.NotFound(nameof(UserProfile), nameof(UserProfile.Auth0Id), auth0Id));
 
             return mapper.Map<UserProfileModel>(userProfile);
         }
@@ -97,10 +75,8 @@ namespace ProfileService.BLL.Services
                 throw new BadRequestException(ExceptionMessages.InvalidId(nameof(UserProfileModel), userId));
             }
 
-            var profileKey = nameof(UserProfile) + userId;
-
-            var profile = await redisCache.GetDataFromCache<UserProfile>(profileKey)
-                ?? await profileRepository.GetByFilterAsync(p => p.Id == userData.Id, cancellationToken)
+            var profile = await userCacheRepositoryManager.GetEntityByIdAsync(
+                userId, updateCache: false, cancellationToken: cancellationToken)
                 ?? throw new NotFoundException(ExceptionMessages.NotFound(nameof(UserProfile), nameof(UserProfile.Id), userData.Id));
 
             if (currentUserAuth0Id != profile.Auth0Id)
@@ -108,14 +84,12 @@ namespace ProfileService.BLL.Services
                 throw new ForbiddenException(ExceptionMessages.AccessDenied(nameof(UserProfile), profile.Id));
             }
 
-            var userToUpdate = mapper.Map<UserProfile>(userData);
+            var profileToUpdate = mapper.Map<UserProfile>(userData);
 
-            profile = await profileRepository.UpdateAsync(userToUpdate, cancellationToken);
+            var updatedProfile = await userCacheRepositoryManager.UpdateEntityAsync(
+                profileToUpdate, [nameof(UserProfile) + userId, nameof(UserProfile.Auth0Id) + profile.Auth0Id], cancellationToken);
 
-            await redisCache.CacheData(profile, TimeSpan.FromMinutes(10), profileKey);
-            await redisCache.CacheData(profile, TimeSpan.FromMinutes(10), nameof(UserProfile.Auth0Id) + profile.Auth0Id);
-
-            return mapper.Map<UserProfileModel>(profile);
+            return mapper.Map<UserProfileModel>(updatedProfile);
         }
     }
 }
