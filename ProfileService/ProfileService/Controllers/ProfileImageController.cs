9fad1f0d38bf5fc6ed7fe381d60d66f31c11c845
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2024-11-13 12:39:24 +0300

    Create profile image controller

---
+++ ProfileService/ProfileService/Controllers/ProfileImageController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ProfileService.BLL.Services.IServices;
+using ProfileService.Extensions;
+
+namespace ProfileService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class ProfileImageController(IProfileImageService profileImageService) : ControllerBase
+    {
+        [HttpPost]
+        public async Task UploadProfileImage(IFormFile file, CancellationToken cancellationToken)
+        {
+            await profileImageService.UploadProfileImageAsync(HttpContext.GetAuth0IdFromContext(), file, cancellationToken);
+        }
+
+        [HttpGet("my")]
+        public async Task<FileStreamResult> GetOwnProfileImage(CancellationToken cancellationToken)
+        {
+            var stream = await profileImageService.GetProfileImageAsync(HttpContext.GetAuth0IdFromContext(), cancellationToken);
+
+            return File(stream, "image/jpeg");
+        }
+
+        [HttpGet("{userId}")]
+        public async Task<FileStreamResult> GetProfileImage(Guid userId, CancellationToken cancellationToken)
+        {
+            var stream = await profileImageService.GetProfileImageAsync(userId, cancellationToken);
+
+            return File(stream, "image/jpeg");
+        }
+    }
+}
