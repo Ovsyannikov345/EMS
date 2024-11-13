2501d806ed29ea04fc8e221d91a49071d24c6b51
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2024-11-13 15:58:39 +0300

    Create estate image service

---
+++ CatalogueService/CatalogueService.BLL/Utilities/Exceptions/Messages/FileExceptionMessages.cs
@@ -0,0 +1,17 @@
+﻿namespace CatalogueService.BLL.Utilities.Exceptions.Messages
+{
+    public static class FileExceptionMessages
+    {
+        public static readonly string EmptyFile = "File is empty or not provided.";
+
+        public static readonly string InvalidFormat = "Invalid file format. Only .jpg, .jpeg, and .png are allowed.";
+
+        public static readonly string FileSizeExceeded = "File size exceeded. Max allowed size is 2 MB.";
+
+        public static readonly string UnknownError = "Error while saving the image.";
+
+        public static readonly string NotFound = "Image not found";
+
+        public static string CountLimitExceeded(int limit) => $"You can't upload more than {limit} files.";
+    }
+}
