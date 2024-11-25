7a6bf8e56c82780a27665fc27ce10e2e8650e85a
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2024-11-25 11:23:27 +0300

    Add profile editing form with validation schema

--- front-end/src/pages/ProfilePage.tsx
+++ front-end/src/pages/ProfilePage.tsx
@@ -7,13 +7,17 @@ import UndoIcon from "@mui/icons-material/Undo";
 import ProfileCard from "../components/ProfileCard";
 import GoBackButton from "../components/buttons/GoBackButton";
 import EditButton from "../components/buttons/EditButton";
+import EditProfileForm from "../components/forms/EditProfileForm";
+import { useNotifications } from "@toolpad/core";
 
 const ProfilePage = () => {
     const { id } = useParams();
 
+    const notifications = useNotifications();
+
     const navigate = useNavigate();
 
-    const { getOwnProfile, getProfile, getProfileImage } = useProfileApi();
+    const { getOwnProfile, getProfile, getProfileImage, updateProfile } = useProfileApi();
 
     const [profile, setProfile] = useState<UserProfile | null>();
 
@@ -22,6 +26,10 @@ const ProfilePage = () => {
     const [isLoading, setIsLoading] = useState(true);
 
     const [error, setError] = useState<string | null>(null);
+
+    const [isEditing, setIsEditing] = useState(false);
+
+    const [reloadProfile, setReloadProfile] = useState(false);
 
     useEffect(() => {
         const loadProfile = async () => {
@@ -50,7 +58,21 @@ const ProfilePage = () => {
         };
 
         loadProfile();
-    }, []);
+    }, [reloadProfile]);
+
+    const handleProfileUpdate = async (updatedProfile: UserProfile) => {
+        const response = await updateProfile(updatedProfile);
+
+        if ("error" in response) {
+            notifications.show("Failed to update profile", { severity: "error", autoHideDuration: 3000 });
+            return;
+        }
+
+        setProfile(response);
+        setIsEditing(false);
+        notifications.show("Profile updated", { severity: "success", autoHideDuration: 3000 });
+        setReloadProfile(!reloadProfile);
+    };
 
     if (isLoading) {
         return (
@@ -81,25 +103,30 @@ const ProfilePage = () => {
         <Container maxWidth="md" sx={{ mt: 2 }}>
             <Grid container justifyContent={"space-between"}>
                 <GoBackButton />
-                {!id && <EditButton onClick={() => console.log("click")} />}
+                {!id && !isEditing && <EditButton onClick={() => setIsEditing(true)} />}
             </Grid>
-
-            <Box sx={{ textAlign: "center", mb: 4 }}>
-                <Avatar
-                    alt={`${profile.firstName} ${profile.lastName}`}
-                    src={`${imageSrc}`}
-                    sx={{ width: 150, height: 150, margin: "0 auto" }}
-                />
-                <Typography variant="h4" sx={{ mt: 2 }}>
-                    {profile.firstName} {profile.lastName}
-                </Typography>
-            </Box>
-            <Grid container spacing={2}>
-                <ProfileCard title="Estate Count" value={profile.estateCount} />
-                <ProfileCard title="Member for" value={moment(profile.createdAt).fromNow(true)} />
-                <ProfileCard title="Birth date" value={moment(profile.birthDate).format("ll")} />
-                <ProfileCard title="Phone number" value={profile.phoneNumber} />
-            </Grid>
+            {isEditing ? (
+                <EditProfileForm initialData={profile} onSubmit={handleProfileUpdate} onCancel={() => setIsEditing(false)} />
+            ) : (
+                <>
+                    <Box sx={{ textAlign: "center", mb: 4 }}>
+                        <Avatar
+                            alt={`${profile.firstName} ${profile.lastName}`}
+                            src={`${imageSrc}`}
+                            sx={{ width: 150, height: 150, margin: "0 auto" }}
+                        />
+                        <Typography variant="h4" sx={{ mt: 2 }}>
+                            {profile.firstName} {profile.lastName}
+                        </Typography>
+                    </Box>
+                    <Grid container spacing={2}>
+                        <ProfileCard title="Estate Count" value={profile.estateCount} />
+                        <ProfileCard title="Member for" value={moment(profile.createdAt).fromNow(true)} />
+                        <ProfileCard title="Birth date" value={moment(profile.birthDate).format("ll")} />
+                        <ProfileCard title="Phone number" value={profile.phoneNumber} />
+                    </Grid>
+                </>
+            )}
         </Container>
     ) : (
         <></>
